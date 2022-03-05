2a9fc41a3d446e8507d1682ccbf82d0219605aa2
Jolow <jolow999@gmail.com>
2022-03-05 23:39:09 +0800

    Add pandas processing to generate humidity and temperature line charts

--- webapp/app.py
+++ webapp/app.py
@@ -4,17 +4,24 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import os
+import pandas as pd 
+from datetime import datetime
 
 def main():
     ref = db.reference("/")
     all = ref.get()
-    power = all["power"]
-    # data, power = all["data"], all["power"]
-    # cur_time = list(data)[-1]
-    # cur = data[cur_time]
+    data, power = all["data"], all["power"]
+    cur_time = list(data)[-1]
+    cur = data[cur_time]
 
-    placeholder = st.empty()
-    placeholder.text(f"Database State: {power}")
+    cur_datetime = datetime.fromtimestamp(int(cur_time))
+    st.text(cur_datetime)
+    
+
+    power_snapshot = st.empty()
+    data_snapshot = st.empty()
+    power_snapshot.text(f"Database State: {power}")
+    data_snapshot.text(f"Data: {cur}")
 
     if st.button("Toggle On or Off"): 
         print(f"{power=}")
@@ -23,32 +30,48 @@ def main():
         elif power =="0": 
             ref.update({"power": "1"})
         new_power = ref.get()["power"]
-        placeholder.text(f"Database State: {new_power}")
+        power_snapshot.text(f"Database State: {new_power}")
 
-    # st.write(cur)
+    if st.button("Update"):
+        new_power =ref.get()["power"]
+        new_data = ref.get()["data"] 
+        new_cur = new_data[list(new_data)[-1]]
+        power_snapshot.text(f"Database State: {new_power}")
+        data_snapshot.text(f"Database State: {new_cur}")
+
+    if st.button("Process Data"):
+        df = pd.DataFrame.from_dict(data, orient='index')
+        # df.index.name = "Time"
+        df = df.reset_index()
+        print(df.keys())
+        df["index"] = df["index"].apply(lambda x: datetime.fromtimestamp(int(x)))
+        df = df.set_index('index')
+        st.dataframe(data=df)
+        st.line_chart(data=df["Humidity"])
+        st.line_chart(data=df["Temperature"])
 
 if __name__ == "__main__":
    
     if not firebase_admin._apps:
-        #  # For LOCAL
-        # cred = credentials.Certificate('cred.json')
-        # default_app = firebase_admin.initialize_app(cred, {
-        #     'databaseURL':"https://temp-humidity-sensor-747b7-default-rtdb.asia-southeast1.firebasedatabase.app" 
-        # })
+         # For LOCAL
+        cred = credentials.Certificate('cred.json')
+        default_app = firebase_admin.initialize_app(cred, {
+            'databaseURL':"https://temp-humidity-sensor-747b7-default-rtdb.asia-southeast1.firebasedatabase.app" 
+        })
 
-        # For PROD
-        default_app = firebase_admin.initialize_app(
-            credentials.Certificate({
-                "type": "service_account",
-                "project_id": os.environ["project_id"],
-                "private_key": os.environ["private_key"],
-                "private_key": os.environ.get('private_key').replace('\\n', '\n'),
-                "client_email": os.environ["client_email"],
-                "token_uri": "https://oauth2.googleapis.com/token",
-            }), 
-            {
-                'databaseURL':"https://temp-humidity-sensor-747b7-default-rtdb.asia-southeast1.firebasedatabase.app" 
-            })
+        # # For PROD
+        # default_app = firebase_admin.initialize_app(
+        #     credentials.Certificate({
+        #         "type": "service_account",
+        #         "project_id": os.environ["project_id"],
+        #         "private_key": os.environ["private_key"],
+        #         "private_key": os.environ.get('private_key').replace('\\n', '\n'),
+        #         "client_email": os.environ["client_email"],
+        #         "token_uri": "https://oauth2.googleapis.com/token",
+        #     }), 
+        #     {
+        #         'databaseURL':"https://temp-humidity-sensor-747b7-default-rtdb.asia-southeast1.firebasedatabase.app" 
+        #     })
     
 
     main()
